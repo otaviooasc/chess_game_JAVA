d7820b8b73591c8c4496f9b0dbfb190c8ca0cb1a
Otávio Cavalcanti <otaviooasc@gmail.com>
2020-06-07 11:35:16 -0200

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package application;
+
+/**
+ * @author Otávio Cavalcanti
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+	}
+}
