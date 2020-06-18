2dffa126c90c27a1df1ebc7f9b7e0147cb395749
Otávio Cavalcanti <otaviooasc@gmail.com>
2020-06-18 20:28:21 -0200

    Incluindo movimentação das peças.

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,12 @@
  */
 package application;
 
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 /**
  * @author Otávio Cavalcanti
@@ -16,8 +21,25 @@ public class Program {
 	 */
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
-		UI.printBoard(chessMatch.getPieces());
+		while(true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+			 	
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
 	}
 }
